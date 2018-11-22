02b94b66c73bf2254a4fee79ef394738241bd14c
Bartosz Sobocki <33702834+bsobocki@users.noreply.github.com>
2018-11-22 22:31:14 +0100

    Update Calculable.java

--- ONP_calculator/tools/Calculable.java
+++ ONP_calculator/tools/Calculable.java
@@ -2,6 +2,7 @@ package tools;
 
 import tools.exceptions.*;
 
+/**objects from class implementing Calculable will be calculating in the interpreter*/
 public interface Calculable {
     double calc () throws ONP_Exception;
 }
